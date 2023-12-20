924651ac37c7ede1c47452a3c2c91bf7f8e74313
mohtoto <totonchym@gmail.com>
2023-12-20 22:23:25 +0800

    feat: added auth to the app

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,18 @@
+export interface children { 
+
+    children: React.ReactNode
+}
+
+const Layout = ({ children } : children) => {
+    return ( 
+
+    <div className="flex-center min-h-screen w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
+        {children}
+    </div>
+
+    )
+
+
+}
+
+export default Layout;
