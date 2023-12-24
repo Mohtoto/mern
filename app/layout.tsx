dddd81ea8be2dc928f2d2bff1c40a4f80a2f742d
mohtoto <totonchym@gmail.com>
2023-12-24 13:52:42 +0800

    feat:added database and schemas

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,32 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
-import './globals.css'
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
 
-const poppins = Poppins({ subsets: ['latin'], weight: ['400' , '500' , '600' , '700'] , variable : '--font-poppins'})
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
-  title: 'EventSphere',
-  description: 'EventSphere is a platform for event management',
-  icons : {
-    icon:'/assets/images/logo.svg'
-  }
-}
+  title: "EventSphere",
+  description: "EventSphere is a platform for event management",
+  icons: {
+    icon: "assets/images/logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={poppins.className}>{children}</body>
-    </html>
+      <html lang="en">
+        <body className={poppins.className}>{children}</body>
+      </html>
     </ClerkProvider>
-  )
+  );
 }
---
+++ lib/database/index.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+
+const MONGO_URI = process.env.MONGODB_URI
+
+let cached = (global as any).mongoose || {conn: null, promsie :null};
+
+
+export const connectToDB = async () => {
+
+if(cached.conn) return cached.conn;
+if(!MONGO_URI) throw new Error('MONGODB URI NOT FOUND')
+
+cached.promsie = cached.promise || mongoose.connect(MONGO_URI , {
+
+    dbName: 'EventSphere',
+    bufferCommands: false
+})
+
+
+cached.conn = await cached.promise;
+
+return cached.conn
+
+}
+
+
+
