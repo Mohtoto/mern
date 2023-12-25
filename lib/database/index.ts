25e516f2f19c35df355bbaad2d9d72aa99ace9c2
mohtoto <totonchym@gmail.com>
2023-12-25 14:11:05 +0800

    sada

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,28 +1,20 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
+const MONGODB_URI = process.env.MONGODB_URI;
 
-const MONGO_URI = process.env.MONGODB_URI
-
-let cached = (global as any).mongoose || {conn: null, promsie :null};
-
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
 
-if(cached.conn) return cached.conn;
-if(!MONGO_URI) throw new Error('MONGODB URI NOT FOUND')
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-cached.promsie = cached.promise || mongoose.connect(MONGO_URI , {
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'evently',
+    bufferCommands: false,
+  })
 
-    dbName: 'events',
-    bufferCommands: false
-})
+  cached.conn = await cached.promise;
 
-
-cached.conn = await cached.promise;
-
-return cached.conn
-
+  return cached.conn;
 }
-
-
-
